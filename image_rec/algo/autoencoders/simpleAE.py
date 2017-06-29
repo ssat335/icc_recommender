32e458a28806d0c7571e633ff8751976aa077baf
ssat335 <ssat335@aucklanduni.ac.nz>
2017-06-29 11:19:39 -0400

    adding image similarity recommender

--- image_rec/algo/autoencoders/simpleAE.py
+++ image_rec/algo/autoencoders/simpleAE.py
@@ -1,47 +1,43 @@
 """
- ConvAE.py  (author: Shameer Sathar /  https://github.com/ssat335)
- 
- Trains a convolutional autoencoder and saves its autoencoder, encoder, and decoder to file.
+ simpleAE.py  (author: Shameer Sathar /  https://github.com/ssat335)
+
+ Trains a simple autoencoder and saves its autoencoder, encoder, and decoder to file.
 """
 # Set path for use on external servers
-import os, sys, time, platform, datetime, pylab
-import numpy as np
+import datetime
+import os
+import platform
+import pylab
+import sys
+import time
+
 import matplotlib.pyplot as plt
-
-project_root = os.path.dirname(__file__)
-sys.path.append(project_root)
-
-from keras.models import load_model
-from keras.datasets import mnist
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
-from keras.models import Model
+import numpy as np
 from keras.callbacks import ModelCheckpoint
+from keras.datasets import mnist
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.models import load_model
 
-from ..utilities.IO import normalize_reshape_img_data
-from ..utilities.IO import load_icc_images_from_dir
-from ..utilities.IO import load_icc_images_from_dir_parallel
-from ..utilities.IO import make_path
-from ..utilities import naming_conventions
+from ..utilities.image_utilities import load_icc_images_from_dir
+from ..utilities.image_utilities import load_icc_images_from_dir_parallel
+from ..utilities.image_utilities import make_path
+from ..utilities.image_utilities import normalize_flatten_img_data
+
 
 def main():
-    name_algo = "ConvAE"
+    project_root = os.path.dirname(__file__)
+    sys.path.append(project_root)
+    name_algo = "simpleAE"
+
     # =================================================
     # Set run parameters
     # =================================================
-    training_dir = "db/icc_images_training"
-
-
-
-    name_data = "women"
-    name_trainingset = "rgb_resized"
-
-    
+    training_dir = os.path.join(project_root, "bin", "")
     gray_scale = 0
     train_model = False  # train and save model?
     n_epochs = 20
-    n_hidden_1 = 64  # 1st hidden layer
-    n_hidden_2 = 32  # 2nd hidden layer
-    n_hidden_3 = 32  # 3rd hidden layer
+    encode_dim = 128
 
 
     n_cores = 32
@@ -57,6 +53,7 @@ def main():
     # =================================================
     # Read and pre-process data
     # =================================================
+    save_models = train_model # save the model?
     dir_dict = make_folder_filename_conventions(name_algo, training_heaven_dir, name_data, name_trainingset)
     if 0:
         for row in dir_dict:
@@ -97,10 +94,10 @@ def main():
             x_train, x_test, index_train, index_test, filenames_list = \
                 load_icc_images_from_dir(training_dir, ratio_training_test, frac_training_use,
                                      n_clone, seed, gray_scale)
-    else:
-        krandom_seed = 100
-        frac_training_use = 1
-        ratio_training_test = 0.8
+    else:  # custom random IO
+        krandom_seed = 101
+        frac_training_use = 0.01
+        ratio_training_test = 0.2
         n_clone = 1
         x_train, x_test, index_train, index_test, filenames_list = \
             load_icc_images_from_dir(training_dir, ratio_training_test, frac_training_use,
@@ -108,21 +105,20 @@ def main():
 
     ypixels = x_train.shape[1]
     xpixels = x_train.shape[2]
-    print(" x_train.shape = {0}".format(x_train.shape))
-    print(" x_test.shape = {0}".format(x_test.shape))
     if gray_scale:
         n_channels = 1
     else:
         n_channels = 3
-    x_train = normalize_reshape_img_data(x_train, n_channels)  # (n, ypixels, xpixels, n_channels)
-    x_test = normalize_reshape_img_data(x_test, n_channels)  # (n, ypixels, xpixels, n_channels)
+    print(" x_train.shape = {0}".format(x_train.shape))
+    print(" x_test.shape = {0}".format(x_test.shape))
+    x_train = normalize_flatten_img_data(x_train)  # (n, total pixels)
+    x_test = normalize_flatten_img_data(x_test)  # (n, total pixels)
     print(" x_train.shape (normalized) = {0}".format(x_train.shape))
     print(" x_test.shape (normalized) = {0}".format(x_test.shape))
 
     # =================================================
     # Train model (or load it)
     # =================================================
-    save_models = train_model # save the model?
     if train_model:
         start_time = time.time()  # start timer
 
@@ -174,26 +170,10 @@ def main():
         run_report.write("\n--- build model architecture start time = {0} ---\n\n".format(time.time() - start_time))
         run_report.close()  # close report
         if 1:
-            convkernel = (3, 3)  # convolution (uses filters): n_filters_1 -> n_filters_2
-            poolkernel = (2, 2)  # pooling (down/up samples image): ypix -> ypix/ypoolkernel, xpix -> xpix/ypoolkernel
-
-            # Create hidden encoding layers
-            input_img = Input(shape=(ypixels, xpixels, n_channels))  # input layer (ypixels, xpixels, n_channels)
-            x = Conv2D(n_hidden_1, convkernel, activation='relu', padding='same')(input_img)
-            x = MaxPooling2D(poolkernel, padding='same')(x)
-            x = Conv2D(n_hidden_2, convkernel, activation='relu', padding='same')(x)
-            x = MaxPooling2D(poolkernel, padding='same')(x)
-            x = Conv2D(n_hidden_3, convkernel, activation='relu', padding='same')(x)
-            encoded = MaxPooling2D(poolkernel, padding='same')(x)  # encoding layer
-
-            # Create hidden decoding layers
-            x = Conv2D(n_hidden_3, convkernel, activation='relu', padding='same')(encoded)
-            x = UpSampling2D(poolkernel)(x)
-            x = Conv2D(n_hidden_2, convkernel, activation='relu', padding='same')(x)
-            x = UpSampling2D(poolkernel)(x)
-            x = Conv2D(n_hidden_1, convkernel, activation='relu')(x)
-            x = UpSampling2D(poolkernel)(x)
-            decoded = Conv2D(n_channels, convkernel, activation='sigmoid', padding='same')(x)  # output layer
+            # Create layers (x_train.shape = (n_train, flattened dim))
+            input_img = Input(shape=(x_train.shape[1],))  # input layer
+            encoded = Dense(encode_dim, activation='relu')(input_img)  # last encoded layer
+            decoded = Dense(x_train.shape[1], activation='sigmoid')(encoded)  # last decoded layer
 
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Set models
@@ -206,22 +186,16 @@ def main():
 
         # Set encoder model
         encoder = Model(input_img, encoded)  # set encoder
-        input_enc_shape = encoder.layers[0].input_shape[1:]  # (ypix, xpix, n_channels)
+        input_enc_shape = encoder.layers[0].input_shape[1:]
         output_enc_shape = encoder.layers[-1].output_shape[1:]
         print(encoder.summary())
 
         # Set decoder model
         decoded_input = Input(shape=output_enc_shape)
-        decoded_output = autoencoder.layers[-7](decoded_input)  # Conv2D
-        decoded_output = autoencoder.layers[-6](decoded_output)  # UpSampling2D
-        decoded_output = autoencoder.layers[-5](decoded_output)  # Conv2D
-        decoded_output = autoencoder.layers[-4](decoded_output)  # UpSampling2D
-        decoded_output = autoencoder.layers[-3](decoded_output)  # Conv2D
-        decoded_output = autoencoder.layers[-2](decoded_output)  # UpSampling2D
-        decoded_output = autoencoder.layers[-1](decoded_output)  # Conv2D
+        decoded_output = autoencoder.layers[-1](decoded_input)
         decoder = Model(decoded_input, decoded_output)
         input_dec_shape = decoder.layers[0].input_shape[1:]
-        output_dec_shape = decoder.layers[-1].output_shape[1:]  # (ypix, xpix, n_channels)
+        output_dec_shape = decoder.layers[-1].output_shape[1:]
         print(decoder.summary())
 
         # Print to report: architecture
@@ -272,22 +246,6 @@ def main():
         run_report.write("\n--- model saving time = {0} ---\n\n".format(time.time() - start_time))
         run_report.close()  # close report
 
-        # Save model
-        if save_models:
-            autoencoder.save(autoencoder_filename)  # save full autoencoder model
-            encoder.save(encoder_filename)  # save full encoder model
-            decoder.save(decoder_filename)  # save full decoder model
-
-            with open(autoencoder_arch_filename, "w+") as json_file:
-                json_file.write(autoencoder.to_json())  # autoencoder arch: json format
-            autoencoder.save_weights(autoencoder_weights_filename)  # autoencoder weights: hdf5 format
-            with open(encoder_arch_filename, "w+") as json_file:
-                json_file.write(encoder.to_json())  # encoder arch: json format
-            encoder.save_weights(encoder_weights_filename)  # encoder weights: hdf5 format
-            with open(decoder_arch_filename, "w+") as json_file:
-                json_file.write(decoder.to_json())  # decoder arch: json format
-            decoder.save_weights(decoder_weights_filename)  # decoder weights: hdf5 format
-
         # Print finished run
         run_report = open(run_report_filename, "a")  # append report
         run_report.write("\n--- full run finished time = {0} ---\n\n".format(time.time() - start_time))
@@ -302,6 +260,22 @@ def main():
         print(autoencoder.summary())
         print(encoder.summary())
         print(decoder.summary())
+
+    # Save model
+    if save_models:
+        autoencoder.save(autoencoder_filename)  # save full autoencoder model
+        encoder.save(encoder_filename)  # save full encoder model
+        decoder.save(decoder_filename)  # save full decoder model
+
+        with open(autoencoder_arch_filename, "w+") as json_file:
+            json_file.write(autoencoder.to_json())  # autoencoder arch: json format
+        autoencoder.save_weights(autoencoder_weights_filename)  # autoencoder weights: hdf5 format
+        with open(encoder_arch_filename, "w+") as json_file:
+            json_file.write(encoder.to_json())  # encoder arch: json format
+        encoder.save_weights(encoder_weights_filename)  # encoder weights: hdf5 format
+        with open(decoder_arch_filename, "w+") as json_file:
+            json_file.write(decoder.to_json())  # decoder arch: json format
+        decoder.save_weights(decoder_weights_filename)  # decoder weights: hdf5 format
 
     # =================================================
     # Predict on our test set
