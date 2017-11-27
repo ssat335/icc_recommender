8b599b2b656a83bd715fb8f63633bc9606208739
ssat335 <ssat335@aucklanduni.ac.nz>
2017-11-27 01:06:27 -0500

    update

--- sim_img_AE/sim_img_AE.py
+++ sim_img_AE/sim_img_AE.py
@@ -1,6 +1,6 @@
 '''
 
- identical_img_rec.py (author: Shameer Sathar / git: https://github.com/ssat335)
+ sim_img_AE.py (author: Shameer Sathar / git: https://github.com/ssat335)
  
  Image identicality recommender system using an autoencoder-clustering model.
  
